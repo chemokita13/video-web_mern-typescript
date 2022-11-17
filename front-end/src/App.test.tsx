15e4ae0c4ea16a4ad31c555ec635e4c695252003
chemokita13 <87540108+chemokita13@users.noreply.github.com>
2022-11-17 21:29:24 +0000

    react fron-end setup

---
+++ front-end/src/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+test('renders learn react link', () => {
+  render(<h1>Hello world!</h1>);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
